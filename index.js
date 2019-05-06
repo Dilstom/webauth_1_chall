59a27fc5a885bd5110b162cbcbfbee386e426028
Dil Sombayeva <dilyasom@gmail.com>
2019-05-06 12:24:59 -0700

    add get req

--- index.js
+++ index.js
@@ -3,11 +3,20 @@ const helmet = require('helmet');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
 
-// const db = require('./database/dbConfig');
+const Users = require('./users/users_model');
 
 const server = express();
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
+server.get('/api/users/', (req, res) => {
+ Users.find()
+  .then(saved => {
+   res.status(201).json(saved);
+  })
+  .catch(err => {
+   res.status(500).json(err);
+  });
+});
 server.listen(5000, () => console.log('App is running on port 5000'));
