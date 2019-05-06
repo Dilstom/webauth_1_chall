eb03ea0fbcd16bc9d4850dfab36c543a02615e72
Dil Sombayeva <dilyasom@gmail.com>
2019-05-06 14:50:33 -0700

    compare passwords pon login

--- index.js
+++ index.js
@@ -34,10 +34,22 @@ server.post('/api/register', (req, res) => {
   });
 });
 
-server.post('/api/register', (req, res) => {
- const user = req.body;
- const hash = bcrypt.hashSync(user.password, 8);
- user.password = hash;
+server.post('/api/login', (req, res) => {
+ const { username, password } = req.body;
+ //  console.log({ username }); // object { username: 'Name'}
+ Users.findBy({ username })
+  .first()
+  .then(user => {
+   if (user && bcrypt.compareSync(password, user.password)) {
+    res.status(200).json({ message: `Welcome ${user.username}` });
+   } else {
+    res.status(401).json({ message: 'Invalid Credentials' });
+   }
+  })
+  .catch(err => {
+   res.status(500).json(err);
+  });
+});
 
  Users.add(user)
   .then(saved => {
